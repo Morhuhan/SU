05c840675383c6efec63d3038212c83e504279f1
morhuhan <radionovich.arkadiy@mail.ru>
2024-04-17 14:35:04 +0500

    .

--- src/main/java/TRPO/SU/Controllers/GlobalExceptionHandler.java
+++ src/main/java/TRPO/SU/Controllers/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package TRPO.SU.Controllers;
 
+import lombok.SneakyThrows;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.dao.DataAccessException;
@@ -12,16 +13,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @SneakyThrows
     @ExceptionHandler(DataAccessException.class)
     public ResponseEntity<?> handleDatabaseException(DataAccessException e) {
         HttpHeaders headers = new HttpHeaders();
         JSONObject responseBody = new JSONObject();
-        try {
-            responseBody.put("error", "Database access error: " + e.getMessage());
-            responseBody.put("status", HttpStatus.BAD_REQUEST.value());
-        } catch (JSONException jsonException) {
-            jsonException.printStackTrace();
-        }
+        responseBody.put("error", "Database access error: " + e.getMessage());
+        responseBody.put("status", HttpStatus.BAD_REQUEST.value());
         return new ResponseEntity<>(responseBody.toString(), headers, HttpStatus.BAD_REQUEST);
     }
 }
